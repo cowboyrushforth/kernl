e9c5fff1c037084a96340db24bb7c782062c4e96
Scott Rushforth <scott@spotman.net>
2013-07-05 15:30:58 -0700

    more cleanups

--- app/controllers/salmon.go
+++ app/controllers/salmon.go
@@ -8,10 +8,7 @@ type Salmon struct {
   Kernl
 }
 
-func (c Salmon) Receive() revel.Result {
-
-  guid := c.Params.Get("guid")
-  raw_xml := c.Params.Get("xml")
+func (c Salmon) Receive(guid string, xml string) revel.Result {
   // get redis handle
   rc := GetRedisConn()
   defer rc.Close()
@@ -21,10 +18,14 @@ func (c Salmon) Receive() revel.Result {
     return c.NotFound("User Not Found")
   }
   revel.INFO.Println("received salmon slap for guid", guid)
-  sane_xml, _ := url.QueryUnescape(raw_xml)
 
+  // urldecode xml var
+  sane_xml, _ := url.QueryUnescape(xml)
+
+  // parse the xml, and verify it
+  // against the senders pubkey
+  // and the users privkey
   verified_payload, err := models.ParseVerifiedSalmonPayload(rc, user, sane_xml)
-
   if err != nil {
     c.Response.Status = 400
     return c.RenderText("")
@@ -32,7 +33,10 @@ func (c Salmon) Receive() revel.Result {
 
   revel.INFO.Println("verified payload:")
   revel.INFO.Println(verified_payload)
+  // TODO: process the payload, take needed actions
 
+  // TODO: return 202 if we had this item already
   c.Response.Status = 200
   return c.RenderText("")
 }
+
