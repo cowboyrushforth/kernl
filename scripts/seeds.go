5d95724c102f106671ef7374f4970ffcea9c7e4b
Andrew Arrow <oneone+github@gmail.com>
2013-07-07 15:48:38 -0700

    progess with loading config

--- scripts/seeds.go
+++ scripts/seeds.go
@@ -2,10 +2,19 @@ package main
 
 import "fmt"
 import "github.com/garyburd/redigo/redis"
+import "github.com/robfig/revel"
 import "kernl/app/models"
 import "code.google.com/p/go.crypto/bcrypt"
 
 func main() {
+  revel.append(ConfPaths, '/Users/aa/scott/src/kernl/conf')
+  config, err := revel.LoadConfig("app.conf")
+  if err != nil {
+    fmt.Println("Hello", err)
+    fmt.Println("Hello", config)
+    return
+  }
+
   c, err := redis.Dial("tcp", "127.0.0.1:6379")
   if err == nil {
     user := models.User{}
