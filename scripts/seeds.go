138eafac424e68737e881edc44fb40f3b5397249
Andrew Arrow <oneone+github@gmail.com>
2013-07-07 15:09:55 -0700

    progress with seed

--- scripts/seeds.go
+++ scripts/seeds.go
@@ -2,10 +2,21 @@ package main
 
 import "fmt"
 import "github.com/garyburd/redigo/redis"
+import "kernl/app/models"
+import "code.google.com/p/go.crypto/bcrypt"
 
 func main() {
-  c, err := redis.Dial("tcp", ":6379")
+  c, err := redis.Dial("tcp", "127.0.0.1:6379")
   if err == nil {
-    fmt.Println("Hello, 世界", c)
+    user := models.User{}
+    user.Slug = "andrew"
+    user.Email = "andrew@aa.com"
+
+    user.PwdHash, _ = bcrypt.GenerateFromPassword([]byte("test"), bcrypt.DefaultCost)
+    fmt.Println("Hello, 世界", user, c)
+    err := user.Insert(c)
+    if err == true {
+      fmt.Println("Hello, 世界", user)
+    }
   }
 }
